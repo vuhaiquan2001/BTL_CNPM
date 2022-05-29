d379198e119877de684a66c1ce2cb562e453c701
vuhaiquan2001 <91311035+vuhaiquan2001@users.noreply.github.com>
2022-05-29 14:38:43 +0700

    Add files via upload

---
+++ QLSP/ConnectMySQL.java
@@ -0,0 +1,43 @@
+package QLSP;
+
+
+import ThongKe.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+/**
+ *
+ * @author HaiQuan
+ */
+public class ConnectMySQL {
+    Connection conn;
+    //ket noi voi database
+    public ConnectMySQL() throws ClassNotFoundException,SQLException{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cnpm","root", "quancap2001");
+   
+    }
+    // lấy thông tin từ mysql
+    public ResultSet getdata(String hoadon) throws SQLException{
+        ResultSet kq=null;
+        Statement stmt = conn.createStatement();
+        String sql ="SELECT * FROM "+hoadon+"";
+        kq=stmt.executeQuery(sql);
+        return kq;
+    }
+    //BẪY LỖI TRÙNG LẶP KHÓA CHÍNH
+    public ResultSet getdata1(String madh) throws SQLException{
+        ResultSet kq=null;
+        Statement stmt = conn.createStatement();
+        String sql ="SELECT MaSP FROM sanpham where MaSP = '"+madh+"' ";
+        kq=stmt.executeQuery(sql);
+        return kq;
+    }
+    //đóng kết nối
+    public void close() throws SQLException{
+        if(this.conn!=null)
+            this.conn.close();
+    }
+}
